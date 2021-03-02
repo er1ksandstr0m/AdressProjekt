bc2161a268b638d2540e5f2a06793f1bca6e37ee
ERIK\Erik <eriksandstrom@outlook.com>
2021-03-02 12:57:49 +0100

    add jdbc test file

--- src/test/java/adressprojekt/JdbcTest.java
+++ src/test/java/adressprojekt/JdbcTest.java
@@ -2,6 +2,8 @@ package adressprojekt;
 
 import static org.apache.camel.component.jms.JmsComponent.jmsComponentClientAcknowledge;
 
+import java.util.List;
+
 import javax.jms.ConnectionFactory;
 import javax.sql.DataSource;
 
@@ -11,63 +13,59 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.impl.JndiRegistry;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.support.DefaultRegistry;
 import org.apache.camel.test.junit4.CamelTestSupport;
-import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.dbcp.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 public class JdbcTest extends CamelTestSupport {
 
-	@Override
-	protected CamelContext createCamelContext() throws Exception {
-		CamelContext camelContext = super.createCamelContext();
+	@Test
+	public void jdbcTimerTest() throws Exception {
+		MockEndpoint mock = getMockEndpoint("mock:result");
+		String url = "jdbc:mysql://localhost:3306/projects";
+		DataSource dataSource = setupDataSource(url);
 
-		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
-		camelContext.addComponent("jms", jmsComponentClientAcknowledge(connectionFactory));
+		DefaultRegistry reg = new DefaultRegistry();
+		reg.bind("myDataSource", dataSource);
 
-		return camelContext;
+		CamelContext context = new DefaultCamelContext(reg);
+		context.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+				from("timer://foo?period=2000").setBody(constant("select * from person;")).to("jdbc:myDataSource")
+				.process(new Processor() {
+
+					public void process(Exchange exchange) throws Exception {
+						
+					}
+					
+				})
+						.to("log:output");
+			}
+		});
+		context.start();
+		Thread.sleep(5000);
+		
+		assertMockEndpointsSatisfied();
+		System.out.println(mock.getExchanges().size());
+//		assertEquals(1, line.get(0));
+//		assertEquals("erik", line.get(1));
+//		assertEquals("persson", line.get(2));
+		context.stop();
+		context.close();
 	}
 
 	private static DataSource setupDataSource(String connectURI) {
 		BasicDataSource ds = new BasicDataSource();
-		ds.setDriverClassName("org.apache.derby.jdbc.EmbeddedDriver");
+		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		ds.setUsername("root");
 		ds.setPassword("Mysecretpw!1");
 		ds.setUrl(connectURI);
 		return ds;
-	}
-	
-	@Test
-	public void jdbcTimerTest() throws Exception {
-		String connectURI = "jdbc:derby:memory:order;create=true";
-		DataSource datasource = setupDataSource(connectURI);
-		
-		DefaultRegistry reg = new DefaultRegistry();
-		reg.bind("myDataSource", null, datasource);
-		context.start();
-		Thread.sleep(5000);
-		context.stop();
-	}
-
-	@Override
-	protected RouteBuilder createRouteBuilder() throws Exception {
-		return new RouteBuilder() {
-
-			@Override
-			public void configure() throws Exception {
-				from("timer://foo?period=2000").process(new Processor() {
-
-					public void process(Exchange exchange) throws Exception {
-
-					}
-				})
-				.to("jdbc:myDataSource").to("log:output");
-			}
-		};
 	}
 }
